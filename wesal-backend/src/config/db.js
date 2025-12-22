f2b2984366a8d71beb8adf808c0422f6f3de5bf1
unknown <ahmedeslam229200@gmail.com>
2025-12-22 17:55:38 +0200

    dsad

--- wesal-backend/src/config/db.js
+++ wesal-backend/src/config/db.js
@@ -1,21 +1,7 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
-
-const connectDB = async () => {
-  if (isConnected) return;
-
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, 
-      connectTimeoutMS: 5000,
-    });
-    isConnected = db.connections[0].readyState;
-    console.log("=> MongoDB Connected");
-  } catch (error) {
-    console.error("=> MongoDB Connection Error:", error.message);
-    throw error; 
-  }
+module.exports = () => {
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
 };
-
-module.exports = connectDB;
